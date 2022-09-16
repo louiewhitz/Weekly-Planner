b2db3866c32a1a9c3f4a67bce46ecac4f245cb53
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-16 21:49:59 +0000

    Added local storage in data.js, added functionality to rendering object function

--- data.js
+++ data.js
@@ -1,9 +1,23 @@
+/* exported data */
+
 var data = {
   view: 'add-entry-page',
   entryId: 1,
-  time: null,
-  day: null,
-  description: '',
-  update: null,
-  entries: []
+  updateEntry: null,
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  currentDay: null
 };
+
+var previousDataJSON = localStorage.getItem('Calendar Entry');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('Calendar Entry', dataJSON);
+});
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* global data */
+
 var modalContainer = document.querySelector('.modal-container');
 var addButton = document.querySelector('#add-entry-button');
 var $formAddEntry = document.querySelector('#modal-form');
@@ -5,16 +7,9 @@ var selectOptionDay = document.querySelector('#days-of-week');
 var selectOptionTime = document.querySelector('#time-of-the-day');
 var description = document.querySelector('#description');
 
-
-
 // addEventListeners
 addButton.addEventListener('click', showModal);
 $formAddEntry.addEventListener('submit', handleEntrySubmit);
-
-
-
-
-
 
 // addEventListener functions
 function showModal(event) {
@@ -25,11 +20,13 @@ function showModal(event) {
 
 function handleEntrySubmit(event) {
   event.preventDefault();
+  var entryId = data.entryId;
+  data.entryId++;
   var currentEntry = {
     day: $formAddEntry.elements.Days.value,
     time: $formAddEntry.elements.Time.value,
-    description: $formAddEntry.elements.description.value
-    entryId: data.nextEntryId;
+    description: $formAddEntry.elements.description.value,
+    entryId
   };
   console.log($formAddEntry.elements);
   console.log('currentEntry', currentEntry);
