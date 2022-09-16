aff716e554b19facda6652177e2fd49a1856417f
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-16 21:23:03 +0000

    Added Submit button function, data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+var data = {
+  view: 'add-entry-page',
+  entryId: 1,
+  time: null,
+  day: null,
+  description: '',
+  update: null,
+  entries: []
+};
--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-var $addEntry = document.querySelector('.addentry');
-var $modal = document.getElementById('modalview');
+var modalContainer = document.querySelector('.modal-container');
+var addButton = document.querySelector('#add-entry-button');
+var $formAddEntry = document.querySelector('#modal-form');
+var selectOptionDay = document.querySelector('#days-of-week');
+var selectOptionTime = document.querySelector('#time-of-the-day');
+var description = document.querySelector('#description');
 
-$addEntry.addEventListener('click', function (event) {
+
+
+// addEventListeners
+addButton.addEventListener('click', showModal);
+$formAddEntry.addEventListener('submit', handleEntrySubmit);
+
+
+
+
+
+
+// addEventListener functions
+function showModal(event) {
   if (event.target.className === 'addentry') {
-    $modal.classList.remove = 'hidden';
+    modalContainer.className = 'modal-container view';
   }
+}
 
-});
+function handleEntrySubmit(event) {
+  event.preventDefault();
+  var currentEntry = {
+    day: $formAddEntry.elements.Days.value,
+    time: $formAddEntry.elements.Time.value,
+    description: $formAddEntry.elements.description.value
+    entryId: data.nextEntryId;
+  };
+  console.log($formAddEntry.elements);
+  console.log('currentEntry', currentEntry);
+  console.dir($formAddEntry);
+
+}
